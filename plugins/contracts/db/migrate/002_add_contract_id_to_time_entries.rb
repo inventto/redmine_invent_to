ebdf1dcb78c47977855dfe4b16c3ac125db2d5ef
Jônatas Davi Paganini <jonatasdp@gmail.com>
2014-01-29 11:56:45 -0200

    plugins removed

--- plugins/contracts/db/migrate/002_add_contract_id_to_time_entries.rb
+++
@@ -1,5 +0,0 @@
-class AddContractIdToTimeEntries < ActiveRecord::Migration
-  def change
-    add_column :time_entries, :contract_id, :integer
-  end
-end
--- plugins/redmine_my_roadmaps/config/routes.rb
+++
@@ -1,3 +0,0 @@
-# encoding: utf-8
-get 'my_roadmaps', :to => 'my_roadmaps#index'
-post 'my_roadmaps', :to => 'my_roadmaps#index'
